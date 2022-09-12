96c667b92d0351d20247e13fcb61ee25c5e3e7e6
Shubham Pandya <59237974+shubham95-de@users.noreply.github.com>
2022-09-12 22:12:31 +0200

    Base File added

--- src/test/java/com/example/mobileautomation/AppiumBasics.java
+++ src/test/java/com/example/mobileautomation/AppiumBasics.java
@@ -1,2 +1,22 @@
-package com.example.mobileautomation;public class AppiumBasics {
+package com.example.mobileautomation;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.Test;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class AppiumBasics extends BaseTest {
+
+    @Test
+    public void WifiSettingsName()throws MalformedURLException {
+
+        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+
+    }
 }
+
+
--- src/test/java/com/example/mobileautomation/BaseTest.java
+++ src/test/java/com/example/mobileautomation/BaseTest.java
@@ -1,2 +1,21 @@
-package com.example.mobileautomation;public class BaseTest {
+package com.example.mobileautomation;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+
+    public AndroidDriver driver;
+    @BeforeClass
+    public void ConfigureAppium() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("Active Emulator pixel 3");
+        options.setApp("//Users//shubhampandya//demo//MobileAutomation//src//test//java//resources//ApiDemos-debug.apk");
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
+    }
+
 }
