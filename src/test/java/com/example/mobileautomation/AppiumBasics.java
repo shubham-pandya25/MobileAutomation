1ec52b6b3c42ae3fe74b70d6cb6da4737dfb52d8
Shubham Pandya <59237974+shubham95-de@users.noreply.github.com>
2022-09-13 01:10:38 +0200

    added new testcase

--- src/test/java/com/example/mobileautomation/AppiumBasics.java
+++ src/test/java/com/example/mobileautomation/AppiumBasics.java
@@ -2,6 +2,8 @@ package com.example.mobileautomation;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -15,7 +17,13 @@ public class AppiumBasics extends BaseTest {
     public void WifiSettingsName()throws MalformedURLException {
 
         driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies\']")).click();
+        driver.findElement(By.id("android:id/checkbox")).click();
+        driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+        String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+        Assert.assertEquals(alertTitle, "WiFi settings");
+        driver.findElement(By.id("android:id/edit")).sendKeys("Shubham Wifi");
+        driver.findElement(By.id("android:id/button1")).click();
     }
 }
 
