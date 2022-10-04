1bdea9796489e609b60eca400bf4a2320a1d829f
Shubham Pandya <59237974+shubham95-de@users.noreply.github.com>
2022-10-04 21:58:44 +0200

    adding new files

--- src/test/java/com/example/mobileautomation/BaseTest.java
+++ src/test/java/com/example/mobileautomation/BaseTest.java
@@ -1,7 +1,11 @@
 package com.example.mobileautomation;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.BeforeClass;
 
 import java.net.MalformedURLException;
@@ -19,5 +23,36 @@ public class BaseTest {
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
+
+    public void longPressAction(WebElement ele)
+    {
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver.executeScript("mobile: longClickGesture",
+                ImmutableMap.of("elementId", ((RemoteWebElement) ele).getId(),
+                        "duration", 2000));
+
+    }
+    public void scrollToEndAction()
+    {
+        boolean canScrollMore;
+        do {
+            canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                    "left", 100, "top", 200, "width", 200, "height", 200,
+                    "direction", "down",
+                    "percent", 3.0
+            ));
+        } while(canScrollMore);
+
+    }
+    public void swipeAction(WebElement ele, String direction)
+    {
+        ((JavascriptExecutor)driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+
+                "elementId", ((RemoteWebElement)ele).getId(),
+                "direction", "left",
+                "percent", 0.75
+        ));
+
+    }
+
 
 }
---
+++ src/test/java/com/example/mobileautomation/ScrollDemo.java
@@ -0,0 +1,27 @@
+package com.example.mobileautomation;
+
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class ScrollDemo extends BaseTest{
+
+    @Test
+
+    public void ScrollDemo() throws MalformedURLException, InterruptedException
+    {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+
+        //where to scroll is known prior
+        //driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));"));
+
+        //No prior idea where to scroll
+
+        scrollToEndAction();
+    }
+
+}
