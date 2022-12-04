5e80beeeb243c2f3ff462a661c57d11948ab3f01
Shubham Pandya <59237974+shubham95-de@users.noreply.github.com>
2022-12-04 23:36:57 +0100

    removed unwanted lines

--- src/test/java/com/example/mobileautomation/DragDrop.java
+++ src/test/java/com/example/mobileautomation/DragDrop.java
@@ -1,11 +1,8 @@
 package com.example.mobileautomation;
 
-import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
