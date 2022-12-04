254cbb47e1cb38ed6d2d7181f58d124f709cd0a5
Shubham Pandya <59237974+shubham95-de@users.noreply.github.com>
2022-12-04 23:36:35 +0100

    practise class added

---
+++ src/test/java/com/example/mobileautomation/practice.java
@@ -0,0 +1,28 @@
+package com.example.mobileautomation;
+
+import io.appium.java_client.AppiumBy;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+ public class practice extends BaseTest {
+
+    @Test
+
+    public void ActionBarTabs() {
+
+        driver.findElement(AppiumBy.accessibilityId("App")).click();
+        driver.findElement(AppiumBy.accessibilityId("Action Bar")).click();
+        driver.findElement(AppiumBy.accessibilityId("Action Bar Tabs")).click();
+
+        String newtab = driver.findElement(AppiumBy.accessibilityId("Add new tab")).getText();
+        String removelasttab = driver.findElement(AppiumBy.accessibilityId("Remove last tab")).getText();
+        String toggletabmode = driver.findElement(AppiumBy.accessibilityId("Toggle tab mode")).getText();
+        String removealltabs = driver.findElement(AppiumBy.accessibilityId("Remove all tabs")).getText();
+
+        Assert.assertEquals(newtab, "Add new tab");
+        Assert.assertEquals(removelasttab, "Remove last tab");
+        Assert.assertEquals(toggletabmode, "Toggle tab mode");
+        Assert.assertEquals(removealltabs, "Remove all tabs");
+    }
+    
+}
