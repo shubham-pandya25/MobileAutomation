008e28bfcd64963e37a2cf4e476cd650436610f5
Shubham Pandya <59237974+shubham95-de@users.noreply.github.com>
2023-01-12 12:33:16 +0530

    added new code

---
+++ src/test/java/com/example/mobileautomation/Learning5.java
@@ -0,0 +1,15 @@
+package com.example.mobileautomation;
+
+public class Learning5 {
+
+
+public String ValidateHeader ()
+{
+
+    System.out.println("I am navigating to Home page");
+    return"Pass";
+
+}
+
+
+}
